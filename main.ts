b71da342fc9336b0fc7ed7ec1b9af989abd4986b
JiangHong5 <jiang_hong_like_5@qq.com>
2026-07-15 22:10:53 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,40 @@
+let interval = 200
+music.ringTone(1320)
+basic.pause(interval)
+music.ringTone(262 * 4)
+basic.pause(interval)
+music.ringTone(1320)
+basic.pause(interval)
+music.ringTone(262 * 4)
+basic.pause(interval)
+music.stopAllSounds()
+basic.forever(function () {
+    basic.showLeds(`
+        . # # # #
+        # . . . .
+        . # # # .
+        . . . . #
+        # # # # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # # # # .
+        # . . . #
+        # # # # .
+        # . . . #
+        # # # # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
